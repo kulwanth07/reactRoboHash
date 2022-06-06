67fed5a657ce57ab1fdf3bb5e37424728f27daac
Kulwanth <kulwanthchinna@gmail.com>
2022-06-06 16:14:27 +0530

    hookCommit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,59 +1,56 @@
 
-import React from "react";
-import { robots } from "../components/robots";
+import React,{useState,useEffect} from "react";
+// import { robots } from "../components/robots";
 import CardList from "../components/CardList";
 import SearchBar from "../components/SearchBar";
 import Scroll from "../components/Scroll";
 import "./App.css"
 
-class App extends React.Component{
-    constructor(){
-        super()
-        this.state = {   
-            robots : [],         
-            searchField:''
-        }
-    }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [iniSearchField, setSearchField] = useState('');
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
         .then(res => res.json())
-        .then(users => this.setState({robots:users}) );
-    }
+        .then(users => {setRobots(users)});
+             
+    },[]);   
+    
 
-    onSearchChange = (event) => {
-        this.setState({searchField:event.target.value})
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value)
     }
+   
 
-    render(){
-        const filteredRobots = robots.filter(rob => {
-            return rob.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    const filteredRobots = robots.filter(rob => {
+            return rob.name.toLowerCase().includes(iniSearchField.toLowerCase());
         })
-
-        if (this.state.robots.length === 0){
-            return   <article class="vh-100 dt w-100">
-            <div class="dtc v-mid tc white ph3 ph4-l">
-              <h1 class="f6 f2-m f-subheadline-l fw6 tc">Loading...Please_Wait!</h1>
+    
+  
+    if (robots.length === 0){
+        return   <article className="vh-100 dt w-100">
+        <div className="dtc v-mid tc white ph3 ph4-l">
+          <h1 className="f6 f2-m f-subheadline-l fw6 tc">Loading...Please_Wait!</h1>
+        </div>
+      </article>
+        // <h1>Loading</h1>
+    } else{
+        return (
+            <div className="tc">
+              <h1 className="f1">RoboHash</h1>
+              <SearchBar searchChange={onSearchChange} />
+              <Scroll>
+              <CardList robots={filteredRobots} />
+                  </Scroll>                  
+             
+              
             </div>
-          </article>
-            // <h1>Loading</h1>
-        } else{
-            return (
-                <div className="tc">
-                  <h1 className="f1">RoboHash</h1>
-                  <SearchBar searchChange={this.onSearchChange} />
-                  <Scroll>
-                  <CardList robots={filteredRobots} />
-                      </Scroll>                  
-                 
-                  
-                </div>
-              );     
-
-        }
+          );     
 
     }
+}
 
-};
 
 export default App;
